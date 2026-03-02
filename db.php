657fe69f95f973a9f693f86a2f3df2f90427ff3a
ARJ <arjcrentoria@gmail.com>
2026-03-02 23:07:25 +0800

    dont add this

--- db.php
+++ db.php
@@ -1,8 +1,9 @@
 <?php
 try {
-    $dbPath = __DIR__ . "/database/glaciers.db";
+   $dbPath = "/home/arj14/glaciers.db";
     $conn = new PDO("sqlite:" . $dbPath);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("PRAGMA foreign_keys = ON");
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
